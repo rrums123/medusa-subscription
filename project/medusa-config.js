0844456d99186777ffb0d13e1a7c94eee4ff352e
Ryan Rumlaklak <ryan.rumlaklak@gmail.com>
2022-05-23 15:34:19 +0700

    removes unnecessary

--- project/medusa-config.js
+++ project/medusa-config.js
@@ -14,8 +14,8 @@ const DATABASE_URL =
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
 // Stripe keys
-const STRIPE_API_KEY = process.env.STRIPE_API_KEY || "sk_test_51KedmEDFUo7wZbIt7ceD43xQ7T4eYKEOUffBJ5ECOuumqFOx6y0riAviO9smqEBQVMLBlQGYXxkNAZhxG7Mg9Knb00Go9Mjo6X";
-const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || "whsec_3c3f55b768a24bb92ed4165cb07c851c247f5e8710eb951294078647b05d9d7e";
+const STRIPE_API_KEY = process.env.STRIPE_API_KEY || "";
+const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || "";
 const RETURN_URL = process.env.RETURN_URL || 'http://localhost:8000'
 // This is the place to include plugins. See API documentation for a thorough guide on plugins.
 const plugins = [
