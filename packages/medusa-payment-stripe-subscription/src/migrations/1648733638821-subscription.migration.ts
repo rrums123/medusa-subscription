243b7f40731023dea757f14ed67c3e7eaf2e0c2a
Ryan Rumlaklak <ryan.rumlaklak@gmail.com>
2022-05-23 15:41:25 +0700

    merge migrations file

--- packages/medusa-payment-stripe-subscription/src/migrations/1648733638821-subscription.migration.ts
+++ packages/medusa-payment-stripe-subscription/src/migrations/1648733638821-subscription.migration.ts
@@ -4,8 +4,8 @@ export class SubscriptionMigration1648733638821 implements MigrationInterface {
     name = 'SubscriptionMigration1648733638821';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE public."subscription" ( "id" TEXT NOT NULL PRIMARY KEY, "status" TEXT NOT NULL, "next_payment_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP WITH TIME ZONE)`);
-        await queryRunner.query(`CREATE TABLE public."subscription_item" ( "id" TEXT NOT NULL PRIMARY KEY , "subscription_id" TEXT NOT NULL, "period" TEXT NOT NULL, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP WITH TIME ZONE)`);
+        await queryRunner.query(`CREATE TABLE public."subscription" ( "id" TEXT NOT NULL PRIMARY KEY, "status" TEXT NOT NULL, "next_payment_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "metadata" jsonb, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP WITH TIME ZONE)`);
+        await queryRunner.query(`CREATE TABLE public."subscription_item" ( "id" TEXT NOT NULL PRIMARY KEY , "subscription_id" TEXT NOT NULL, "period" TEXT NOT NULL, "metadata" jsonb, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "deleted_at" TIMESTAMP WITH TIME ZONE)`);
         await queryRunner.query(`ALTER TABLE public."cart" ADD COLUMN IF NOT EXISTS "subscription_id" TEXT DEFAULT NULL`)
         await queryRunner.query(`ALTER TABLE public."line_item" ADD COLUMN IF NOT EXISTS "subscription_item_id" TEXT DEFAULT NULL`)
         await queryRunner.query(`ALTER TABLE public."product_variant" ADD COLUMN IF NOT EXISTS "is_subscription" boolean DEFAULT false`)
